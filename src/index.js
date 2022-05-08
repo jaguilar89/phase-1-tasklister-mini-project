4c6e0687229636b7f960814ffef464abcb2c496e
Jose Aguilar <jaguilarny@gmail.com>
2022-05-08 19:21:09 -0400

    created "add" and "delete" function for tasks

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form');
+  const list = document.querySelector('#list')
+  form.addEventListener('submit', addTask)
+  list.addEventListener('click', removeTask)
 });
+
+function addTask(event) {
+  event.preventDefault()
+  const tasks = document.querySelector('#tasks');
+  let li = document.createElement('li');
+  li.textContent = `${event.target.new_todo.value} `;
+
+  let deleteButton = document.createElement('button')
+  deleteButton.className = 'delete-btn';
+  deleteButton.textContent = 'remove';
+  
+  li.appendChild(deleteButton);
+  tasks.appendChild(li)
+}
+
+function removeTask(event) {
+  if (event.target.className === 'delete-btn') {
+    event.target.parentNode.remove();
+  }
+}
